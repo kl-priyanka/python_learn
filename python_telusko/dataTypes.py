435322308afc80fbbd1e6390ff37aaf2ae87bed0
kl-priyanka <priyankz432@gmail.com>
2024-09-04 16:07:37 +0530

    04.09.224(1)

---
+++ python_telusko/dataTypes.py
@@ -0,0 +1,3 @@
+#none, numeric, list, tuple, set, string, range, dictionary are the datatypes
+#none : when a variable is not assigned with any value; similar to null
+#numeric : int, float, complex, bool
